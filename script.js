6aef907380a038a3c459b3ef3f857f6135e9e178
Dhwanil03 <iamdhwanilrokad@gmail.com>
2022-07-19 12:19:50 +0530

    commit5

--- script.js
+++ script.js
@@ -89,6 +89,80 @@ d3.csv(chartdata).then(function(datapoints) {
    }
            
        }); 
+       const ctx1= document.getElementById('myChart1').getContext('2d');
+      
+       const myChart1 = new Chart(ctx1,{
+           type:'bar',
+           data:{
+               labels:['Max Verstappen', 'Lewis Hamilton', 'Valtteri Bottas', 'Sergio Perez', 'Carlos Sainz',
+                       'Lando Norris', 'Charles Leclerc',  'Daniel Ricciardo', 'Pierre Gasly',  'Fernando Alonso',
+                       'Esteban Ocon', 'Sebastian Vettel',  'Lance Stroll', 'Yuki Tsunoda', 'George Russell',
+                       'Kimi Räikkönen', 'Nicholas Latifi', 'Antonio Giovinazzi', 'Mick Schumacher',  'Robert	Kubica',
+                       'Nikita	Mazepin'],
+               datasets:[{
+                   label:'Laps Lead',
+                   data: LapsLead,
+                           backgroundColor:[
+                           'rgba(6, 0, 254, 0.98)',
+                           'rgba(6, 229, 254, 0.8)',
+                           'rgba(6, 0, 254, 0.98)',
+                           'rgba(6, 229, 254, 0.8)',
+                           'rgba(146, 0, 0, 10)',
+                           'rgba(255, 169, 0, 0.98)',
+                           'rgba(146, 0, 0, 10)',
+                           'rgba(255, 169, 0, 0.98)',
+                           'rgba(0, 0, 148, 0.39)',
+                           'rgba(0, 0, 104, 1)',
+                           'rgba(0, 0, 104, 1)',
+                           'rgba(0, 116, 0, 0.98)',
+                           'rgba(0, 116, 0, 0.98)',
+                           'rgba(0, 0, 148, 0.39)',
+                           'rgba(46, 164, 210, 0.75)',
+                           'rgba(146, 0, 0, 1)',
+                           'rgba(46, 164, 210, 0.75)',
+                           'rgba(146, 0, 0, 1)',
+                           'rgba(175, 0, 66, 0.75)',
+                           'rgba(146, 0, 0, 1)',
+                           'rgba(175, 0, 66, 0.75)'
+                           ]
+               }]
+           },
+           options: {
+
+               scales:{
+                x:{ ticks:{
+                        color:'white'
+                    
+                    }
+
+                },
+                y:{
+                    ticks:{
+                        color:'white'
+                    
+                    }
+                  
+                }
+            },
+              
+             indexAxis:'y',  
+           plugins: {
+               
+           title: {
+               display: true,
+               text: 'Laps Lead',
+               color:'white'
+           },
+         
+           legend: {
+               display: false
+               
+
+           }
+       }
+   }
+           
+       }); 
        const ctx2= document.getElementById('myChart2').getContext('2d');
     const myChart2 = new Chart(ctx2,{
    type:'line',
